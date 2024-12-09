d6668479e7bbd0d2f6a5d802e7763e8160f1eac6
Shriya Gautam <57238679+bits-and-bytes85@users.noreply.github.com>
2024-12-09 11:58:35 -0500

    Update GrammarSolver.java

--- GrammarSolver.java
+++ GrammarSolver.java
@@ -1,8 +1,4 @@
-/*
-Shriya Gautam
-CSE 143, TA: Yafqa Khan
-The GrammarSolver class takes in a list of grammar rules in Backus-Naur Form and allows the client to perform tasks with it. The client can print out the symbols in the grammar, check if a symbol is in the grammer, and generate instances of a symbol
-*/
+
 import java.io.*;
 import java.util.*;
 
